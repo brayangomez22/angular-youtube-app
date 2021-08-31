0a404b45be0eec44240a72271962df892a6dbd97
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-08-30 21:59:33 -0500

    Showing the videos on cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
+import { Video } from '../../models/youtube.models';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,13 @@ import { YoutubeService } from '../../services/youtube.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  videos: Video[] = [];
+
   constructor(private youtubeService: YoutubeService) {}
 
   ngOnInit(): void {
     this.youtubeService.getVideos().subscribe((resp) => {
-      console.log(resp);
+      this.videos.push(...resp);
     });
   }
 }
