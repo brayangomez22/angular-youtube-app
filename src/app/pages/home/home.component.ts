55ffb86b69acdb06709aea8c7608946317d15c68
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-08-30 22:12:23 -0500

    Show a modal with the video

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
 import { Video } from '../../models/youtube.models';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -15,6 +17,27 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.youtubeService.getVideos().subscribe((resp) => {
       this.videos.push(...resp);
+    });
+  }
+
+  showVideo(video: Video) {
+    Swal.fire({
+      html: `
+      <h4>${video.title}</h4>
+      <hr>
+      <iframe width="100%" 
+              height="315" 
+              src="https://www.youtube.com/embed/${video.resourceId.videoId}" 
+              title="YouTube video player" 
+              frameborder="0" 
+              allow="accelerometer; 
+              autoplay; 
+              clipboard-write; 
+              encrypted-media; 
+              gyroscope; 
+              picture-in-picture" 
+              allowfullscreen>
+      </iframe>`,
     });
   }
 }
